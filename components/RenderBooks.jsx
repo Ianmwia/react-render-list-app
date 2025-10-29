ff05493a0dbc73b38321b8c55c74cfcf2b36ab43
Mwia <iansnr10@gmail.com>
2025-10-29 15:20:03 +0300

    styling the cards and adding an image

--- components/RenderBooks.jsx
+++ components/RenderBooks.jsx
@@ -31,13 +31,14 @@ export default function RenderBooks(){
 
     }
     return(
-      <div>
+      <div className='m-2.5'>
         <h1 className='text-3xl text-green-900'>Books</h1>
-        <div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4'>
             {books.map((book) =>(
-                <div key={book.key} className=''>
-                    <p>{book.title}</p>
-                    <p>{book.author_name}</p>
+                <div key={book.key} className='flex flex-col justify-between rounded mb-2.5 gap-2 border-gray-200 bg-zinc-200 shadow-lg hover:shadow-xl transition-shadow duration-300 p-4'>
+                    <img className='rounded mb-2' src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`} alt={book.title} />
+                    <p className='text-lg font-medium text-black truncate' >{book.title}</p>
+                    <p className='text-sm text-gray-600'>Author: {book.author_name}</p>
                 </div>
             ))}
         </div>
